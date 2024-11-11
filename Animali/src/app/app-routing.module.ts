6fa519a6f497988d3bf89af26780da94832e0311
Antonio Soliman <soliman.antonio@iisgalvanimi.edu.it>
2024-11-11 13:12:29 +0000

    Parte 1 fatto

--- Animali/src/app/app-routing.module.ts
+++ Animali/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AnimaliComponent } from './animali/animali.component';
+import { FruttiComponent } from './frutti/frutti.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'animals', component: AnimaliComponent},
+  { path: 'fruits', component: FruttiComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Animali/src/app/app.module.ts
+++ Animali/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AnimaliComponent } from './animali/animali.component';
+import { FruttiComponent } from './frutti/frutti.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnimaliComponent,
+    FruttiComponent
   ],
   imports: [
     BrowserModule,
