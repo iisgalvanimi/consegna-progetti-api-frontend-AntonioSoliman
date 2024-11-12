fc17f296b5534efd4ee67566ae9ae151d08bf340
Antonio Soliman <soliman.antonio@iisgalvanimi.edu.it>
2024-11-12 09:30:04 +0000

    Completamento parte 1

--- Animali/src/app/login/login.component.ts
+++ Animali/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { animals, Emoji, fruits } from '../models/emoji.model';
 
 @Component({
   selector: 'app-login',
@@ -7,14 +8,21 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  genVect : Emoji[]= [{name:"", emoji:""}];
+  //Riceve l'oggetto ActivatedRoute come dependency injection
   constructor(private route: ActivatedRoute) {
+    //Gestisce i cambi di route con l'observable paramMap
     this.route.paramMap.subscribe(this.getRouterParam);
   }
-
+ 
+  //Ogni volta che viene invocata la route tracks/:id, l'observable paramMap richiama questo metodo
   getRouterParam = (params: ParamMap) =>
-    {
-      let uri_param = params.get('id'); //Ottengo l'id dalla ParamMap
-      console.log (uri_param); //Stampo su console
-      //this.service.getTrack()
-    }
+  {
+    let uri_param = params.get('id'); //Ottengo l'id dalla ParamMap
+    console.log (uri_param); //Stampo su console
+    //this.service.getTrack()
+    if (uri_param == 'fruits') this.genVect = fruits;
+    if (uri_param == 'animals') this.genVect = animals;
+  }
+
 }
