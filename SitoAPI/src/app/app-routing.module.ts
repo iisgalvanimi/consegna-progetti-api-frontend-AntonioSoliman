ea8b5c57ef539465b51f826213c51b78a00d72cc
Antonio Soliman <soliman.antonio@iisgalvanimi.edu.it>
2024-11-15 11:02:54 +0000

    continuazione 1

--- SitoAPI/src/app/app-routing.module.ts
+++ SitoAPI/src/app/app-routing.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PrimoComponent } from './primo/primo.component';
-import { SecondoComponent } from './secondo/secondo.component';
-import { TerzoComponent } from './terzo/terzo.component';
-import { QuartoComponent } from './quarto/quarto.component';
-import { QuintoComponent } from './quinto/quinto.component';
+import { CardComponent } from './card/card.component';
+
 
 
 const routes: Routes = [
-  { path: 'primo', component: PrimoComponent},
-  { path: 'secondo', component: SecondoComponent},
-  { path: 'terzo', component: TerzoComponent},
-  { path: 'quarto', component: QuartoComponent},
-  { path: 'quinto', component: QuintoComponent},
+  { path: ':name', component: CardComponent}
 ];
 
 @NgModule({
--- SitoAPI/src/app/app.module.ts
+++ SitoAPI/src/app/app.module.ts
@@ -3,20 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PrimoComponent } from './primo/primo.component';
-import { SecondoComponent } from './secondo/secondo.component';
-import { TerzoComponent } from './terzo/terzo.component';
-import { QuartoComponent } from './quarto/quarto.component';
-import { QuintoComponent } from './quinto/quinto.component';
+import { CardComponent } from './card/card.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrimoComponent,
-    SecondoComponent,
-    TerzoComponent,
-    QuartoComponent,
-    QuintoComponent
+    CardComponent
   ],
   imports: [
     BrowserModule,
